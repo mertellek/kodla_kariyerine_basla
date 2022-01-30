5c61f2a76e97819425f832a285b2465e5deef787
Mert Ellek <mertellek@hotmail.com>
2022-01-30 16:14:29 +0300

    first commit

---
+++ hafta2_odev1/hafta2_odev1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace hafta2_odev1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Adınızı Giriniz: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınızı Giriniz: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldiniz " + name + " " + surname + ".");
+            Console.ReadKey();
+
+        }
+    }
+}
