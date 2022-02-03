a4bcbe399af71539f31a65fea565af4095f6adf8
Mert Ellek <mertellek@hotmail.com>
2022-02-03 17:57:53 +0300

    hata_yonetimi
    hata_yonetimi

---
+++ hata_yonetimi/hata_yonetimi/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace hata_yonetimi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* try
+            {
+                Console.WriteLine("Bir sayı giriniz: ");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Girdiğiniz sayi: " + sayi);
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Hata: " + ex.Message.ToString());
+            }           
+            finally
+            {
+                Console.WriteLine("İşlem başarıyla tamamlandı.");
+            }
+            */
+           
+            try
+            {
+                //int a = int.Parse(null);
+                //int b = int.Parse("test");
+                int c = int.Parse("-12345678900");
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Boş değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Veri tipi uygun değil.");
+                Console.WriteLine(ex);
+            }
+            catch(OverflowException ex)
+            {
+                Console.WriteLine("Çok küçük ya da çok büyük bir değer girdiniz.");
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("işlem başarıyla tamamlandı.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
